58e9ad2fd92e8d838accb93b55ee99a42154677c
byBISON <so.shock.k.bison@gmail.com>
2024-07-28 16:56:05 +0900

    swift-tools-version:6.0で動かなかったので修正

--- ch1_nocode_lowcode/05/Trail/Packages/RealityKitContent/Package.swift
+++ ch1_nocode_lowcode/05/Trail/Packages/RealityKitContent/Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RealityKitContent",
-    platforms: [
-        .visionOS(.v2),
-        .macOS(.v15),
-        .iOS(.v18)
-    ],
+//    platforms: [
+//        .visionOS(.v2),
+//        .macOS(.v15),
+//        .iOS(.v18)
+//    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
